a269f77a5b3c46b5100ecc85991a663ce8948680
Youralexey <johndoe@example.com>
2021-10-12 20:00:58 +0300

    Updates

--- static/js/nofound.c72dc068.chunk.js
+++ static/js/nofound.c72dc068.chunk.js
@@ -1,2 +1,2 @@
 (this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[6],{69:function(t,e,i){"use strict";i.r(e);i(0);var n=i(1);e.default=function(){return Object(n.jsx)("h2",{children:"404 - Page not found"})}}}]);
-//# sourceMappingURL=nofound.bf1ba0cd.chunk.js.map
+//# sourceMappingURL=nofound.c72dc068.chunk.js.map
